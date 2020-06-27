8e3ff5f4d18c08409b93ed78942fcccb7a71e31a
bhs8574 <bhs8574@gmail.com>
2020-06-26 20:24:55 -0500

    Finished part 1: setup, started on part 2.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
 ## Part 1: Test it with SQL
+SELECT column_name, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE table_name = 'job';
+
+## Gives employer varchar, id int, name varchar, skills varchar.
+
+
 
 ## Part 2: Test it with SQL
 
+
+
 ## Part 3: Test it with SQL
+
+
 
 ## Part 4: Test it with SQL
